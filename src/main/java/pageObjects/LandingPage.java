a94eafaa22a4be8d93184e95b0c199e669c957cb
apeksha <apekshahpatel@gmail.com>
2023-02-05 14:20:53 -0500

    usa guys did some work

--- src/main/java/pageObjects/LandingPage.java
+++ src/main/java/pageObjects/LandingPage.java
@@ -18,6 +18,12 @@ public class LandingPage {
 		 System.out.println("postJira3");
 		 System.out.println("postJira4");
 		 System.out.println("postJira5");
+		 
+		 System.out.println("USA1");
+		 System.out.println("USA guy");
+		 System.out.println("USA111");
+		 System.out.println("USA11");
+		 System.out.println("USA11");
 
 	}
 
