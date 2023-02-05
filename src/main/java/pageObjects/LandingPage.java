656884061488d4f97e3e7ba4767da4dd5b68b450
apeksha <apekshahpatel@gmail.com>
2023-02-05 13:38:25 -0500

    second commit

--- src/main/java/pageObjects/LandingPage.java
+++ src/main/java/pageObjects/LandingPage.java
@@ -1,5 +1,16 @@
 package pageObjects;
 
-public class LandingPage {
+import java.time.Duration;
 
-}
+public class LandingPage {
+ 
+	 private void syso() {
+		// TODO Auto-generated method stub
+		 
+		 System.out.println("postJira");
+		 System.out.println("postJira2");
+		 System.out.println("postJira3");
+
+	}
+
+	}
