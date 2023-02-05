9402ba1ca6e9a0464a2c32c8d421afcf5f6d8285
apeksha <apekshahpatel@gmail.com>
2023-02-05 13:48:55 -0500

    two files added

--- src/main/java/pageObjects/LandingPage.java
+++ src/main/java/pageObjects/LandingPage.java
@@ -10,6 +10,8 @@ public class LandingPage {
 		 System.out.println("postJira");
 		 System.out.println("postJira2");
 		 System.out.println("postJira3");
+		 System.out.println("postJira4");
+		 System.out.println("postJira5");
 
 	}
 
